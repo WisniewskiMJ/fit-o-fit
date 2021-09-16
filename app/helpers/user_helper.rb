682940bb3bdea59a54fa148631ad44057ad5d431
Bakshee <bakshee.jw@gmail.com>
2021-09-16 23:54:15 +0200

    Style user dashboard

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -6,6 +6,11 @@ module UserHelper
 
   def month_kilometers(user)
     Activity.where('day >= ? and user_id = ?', Date.today.beginning_of_month, user.id)
+            .sum(:distance)                   
+  end
+
+  def month_kilometers_daily(user)
+    Activity.where('day >= ? and user_id = ?', Date.today.beginning_of_month, user.id)
             .group(:day)
             .sum(:distance)                   
   end
