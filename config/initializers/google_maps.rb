7042d0d0198bb1eb2a898a03a1aafd126aefadb0
WisniewskiMJ <bakshee.jw@gmail.com>
2024-11-11 03:11:29 +0100

    Fix google maps

--- config/initializers/google_maps.rb
+++ config/initializers/google_maps.rb
@@ -1,5 +1,5 @@
-Google::Maps.configure do |config|
-  config.authentication_mode = Google::Maps::Configuration::API_KEY
-  config.api_key = ENV['GMAPS_API_KEY']
-  config.default_language = :pl
-end
+# Google::Maps.configure do |config|
+#   config.authentication_mode = Google::Maps::Configuration::API_KEY
+#   config.api_key = ENV['GMAPS_API_KEY']
+#   config.default_language = :pl
+# end
